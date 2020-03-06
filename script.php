6cdb729fd4de6918fd3016f80e9e5cd2ac56ed65
Rajpreet Dhaliwal <Rajpreet9910@gmail.com>
2020-03-06 13:24:31 -0500

    Created assioate array and fixed html page

---
+++ script.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Rajpreet Dhaliwal
+3/6/2020
+*/
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$terms = array(
+    "
+ajax"=>"a Greek hero in the Trojan War who rescued the body of Achilles
+and killed
+himself out of jealousy when Odysseus was awarded the armor
+of Achilles.",
+    "button"=>"a small disk, knob, or the like for sewing or otherwise
+attaching to an article, as of clothing, serving as a fastening when passed
+through a buttonhole or loop.",
+    "load" =>"anything put in or on something for conveyance or
+transportation; freight; cargo.",
+    "java" =>"a high-level programming language developed by Sun Microsystems.",
+    "php" =>"an HTML-embedded Web scripting language."
+);
