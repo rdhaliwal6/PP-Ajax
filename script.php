63c8677c4a754fc0bd804db9301508c533faa600
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-03-06 14:10:10 -0500

    Trying to implement ajax call with php script. Functionality still needs a fix.

--- script.php
+++ script.php
@@ -6,18 +6,24 @@ Rajpreet Dhaliwal
 //turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+var_dump($_POST);
+$term = $_POST['terms'];
+searchFor($term);
 
-$terms = array(
-    "
-ajax"=>"a Greek hero in the Trojan War who rescued the body of Achilles
-and killed
+function searchFor($term)
+{
+    $terms = array(
+        "ajax" => "a Greek hero in the Trojan War who rescued the body of Achilles
+    and killed
 himself out of jealousy when Odysseus was awarded the armor
 of Achilles.",
-    "button"=>"a small disk, knob, or the like for sewing or otherwise
+        "button" => "a small disk, knob, or the like for sewing or otherwise
 attaching to an article, as of clothing, serving as a fastening when passed
 through a buttonhole or loop.",
-    "load" =>"anything put in or on something for conveyance or
+        "load" => "anything put in or on something for conveyance or
 transportation; freight; cargo.",
-    "java" =>"a high-level programming language developed by Sun Microsystems.",
-    "php" =>"an HTML-embedded Web scripting language."
-);
+        "java" => "a high-level programming language developed by Sun Microsystems.",
+        "php" => "an HTML-embedded Web scripting language."
+    );
+    return $terms[array_search($term, $terms)];
+}
